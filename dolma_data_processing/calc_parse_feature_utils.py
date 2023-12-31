da535b73eacb3d49b6110a4cb771078e03ee3c68
lindiatjuatja <lindiat@andrew.cmu.edu>
2023-12-30 22:11:59 -0500

    dep parse features

--- dolma_data_processing/calc_parse_feature_utils.py
+++ dolma_data_processing/calc_parse_feature_utils.py
@@ -1,5 +1,6 @@
+import numpy as np
 import stanza
-from typing import List, Dict, Tuple, Union, Optional, NewType
+from typing import List, Dict, Tuple, Any, Union, Optional, NewType
 
 ### Parse-based features (word + sentence level)
 # TODO: map back to token-level
@@ -12,18 +13,31 @@ from typing import List, Dict, Tuple, Union, Optional, NewType
 # *5. Number of words in input 
 # *6. Number of sentences in input (num_sentences)
 ## Dependency parse
-# 1. Depth of word
-# 2. Depth of tree
-# 3. Distance of word from immediate head
+# 1. Distance of word from immediate head
+# 2. Distance of word to root
 
 FeatureDict = NewType("FeatureDict", Dict[str, List[Union[str, int, float]]])
 
 
-def get_const_parse_features(input_text: str) -> FeatureDict:
+def get_const_parse_features(
+    input_text: str,
+    pipeline: stanza.Pipeline,
+) -> FeatureDict:
     """
-    Takes as input a chunk of text (paragraph, document)
+    Takes as input a chunk of text (paragraph, document) and stanza pipeline
+    Lang for the pipeline should be 'en', processors are 'tokenize,pos,constituency'
     Returns a feature dict, where values are lists features per word
     """
+    feature_dict = FeatureDict({
+            "const_word_depth": [],
+            "const_tree_depth": [],
+            "upos_label": [],
+            "xpos_label": [],
+            "num_words_sentence": [],
+            "num_words_input": [],
+            "num_sentences_input": [],
+        })
+
     def get_depth_features(
         tree: stanza.constituency,
     ) -> Tuple[List[int], List[int]]:
@@ -45,21 +59,12 @@ def get_const_parse_features(input_text: str) -> FeatureDict:
         max_depth = [max(word_depths)] * len(word_depths)
         return word_depths, max_depth
 
-    feature_dict = FeatureDict({
-        "const_word_depth": [],
-        "const_tree_depth": [],
-        "upos_label": [],
-        "xpos_label": [],
-        "num_words_sentence": [],
-        "num_words_input": [],
-        "num_sentences_input": [],
-    })
-    pipeline = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency')
+    # pipeline = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency')
     processed_text = pipeline(input_text)
     num_words_input = 0
     for sentence in processed_text.sentences:
         num_words_input += len(sentence.words)
-        for word in sentence:
+        for word in sentence.words:
             feature_dict.num_sentences_input.append(len(processed_text.sentences))
             feature_dict.num_words_sentence.append(len(sentence.words))
             feature_dict.upos_label.append(word.upos)
@@ -68,4 +73,36 @@ def get_const_parse_features(input_text: str) -> FeatureDict:
         feature_dict.const_word_depth += word_depths
         feature_dict.const_tree_depth += max_depth
     feature_dict.num_words_input = [num_words_input] * num_words_input
+    return feature_dict
+
+
+def get_dep_parse_features(
+    input_text: str,
+    pipeline: stanza.Pipeline,
+) -> FeatureDict:
+    """
+    Takes as input a chunk of text (paragraph, document) and stanza pipeline
+    Lang for the pipeline should be 'en', processors are 'tokenize,mwt,pos,lemma,depparse'
+    Returns a feature dict, where values are lists features per word
+    """
+    feature_dict = FeatureDict({
+        "dist_to_head": [],
+        "dist_to_root": []
+    })
+    processed_text = pipeline(input_text)
+    for sentence in processed_text.sentences:
+        root_dist = np.zeros(len(sentence.words), 1)
+        head_dist = []
+        root = -1
+        for i, word in enumerate(sentence.words):
+            root_dist[i] = word.id
+            if word.head == 0:
+                head_dist.append(0)
+            else:
+                head_dist.append(abs(word.id - word.head))
+            if word.head == 0:
+                root = word.id
+        root_dist -= root
+        feature_dict.dist_to_head += head_dist
+        feature_dict.dist_to_root += root_dist.tolist()
     return feature_dict
