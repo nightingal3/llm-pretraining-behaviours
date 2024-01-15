f98183a0fe09953c0cc52e35735c2a15e9ec11d6
nightingal3 <emmy.liu.1998@gmail.com>
2024-01-15 14:01:31 -0500

    Refine demo scripts (#11)
    * enable fetch_tokens_from_dolma to run without specifying domain
    
    * debug dolma download (broken)
    
    * fix issue with saving chunks
    
    * fix issue with saving chunks
    
    * add option to fetch greater number of tokens easily
    
    * add sbatch token collection script
    
    * black format
    
    * black format
    
    * fix index bug
    
    * black formatting
    
    * black
    
    ---------
    
    Co-authored-by: Michael Chen <chen.michael.1116@gmail.com>

--- demo_scripts/fetch_tokens_from_dolma.py
+++ demo_scripts/fetch_tokens_from_dolma.py
@@ -9,6 +9,7 @@ from io import BytesIO
 import pyarrow
 import pandas as pd
 import os
+import logging
 from tqdm import tqdm
 
 LLAMA_DIR = "/data/datasets/models/huggingface/meta-llama/Llama-2-70b-hf/"
@@ -22,6 +23,22 @@ TOKENS_TO_FETCH_10B = {
     "wiki-en-simple": 200_000_000,
 }
 
+DUMP_FREQUENCY = 1_000_000
+
+
+def parse_num(val: str) -> int:
+    if val.lower().endswith("b"):
+        return int(val[:-1]) * 1_000_000_000
+    elif val.lower().endswith("m"):
+        return int(val[:-1]) * 1_000_000
+    else:
+        try:
+            return int(float(val))
+        except:
+            raise ValueError(
+                "You must pass either an integer, scientific notation, or xB/xM for num tokens"
+            )
+
 
 def process_zipped_file(content: bytes) -> list:
     with gzip.open(BytesIO(content), "rt") as f:
@@ -30,10 +47,94 @@ def process_zipped_file(content: bytes) -> list:
         return lines
 
 
+def fetch_tokens(
+    num_tokens: int, domain: str, output_dir: str or None, all_files_lst: list
+):
+    current_tokens = 0
+    output_dir = output_dir if output_dir else f"./dolma/{domain}_{num_tokens}"
+    logging.info(f"Fetching {num_tokens} tokens from {domain}")
+
+    # shuffle
+    random.seed(42)
+    random.shuffle(all_files_lst)
+    all_texts = []
+
+    # filter out non-gz files
+    all_files_lst = [f for f in all_files_lst if f.endswith(".gz")]
+
+    # filter by top level domain
+    all_files_lst = [f for f in all_files_lst if domain in f]
+    tokenizer = AutoTokenizer.from_pretrained(LLAMA_DIR)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    file_ind = 0
+    part_ind = 0
+    with tqdm(total=num_tokens) as pbar:
+        while current_tokens < num_tokens and file_ind < len(all_files_lst):
+            response = requests.get(
+                f"http://128.2.209.71:5000/{all_files_lst[file_ind]}"
+            )
+
+            if response.status_code != 200:
+                logging.info(f"Error fetching {all_files_lst[file_ind]}")
+                continue
+
+            file_ind += 1
+
+            docs = [json.loads(l) for l in process_zipped_file(response.content)]
+            texts = [d["text"] for d in docs]
+
+            # tokenizing individually to avoid oom
+            for i, text in enumerate(texts):
+                all_texts.append(docs[i])
+                encoded_inputs = tokenizer(
+                    text, truncation=True, padding=True, return_tensors="pt"
+                )
+                num_non_padding_toks = (
+                    encoded_inputs["attention_mask"].sum(dim=1).tolist()
+                )
+                current_tokens += sum(num_non_padding_toks)
+                pbar.update(sum(num_non_padding_toks))
+
+                # save the reduced dataset as an arrow file, dump every 1M lines
+                if current_tokens >= num_tokens or len(all_texts) >= DUMP_FREQUENCY:
+                    part_ind += 1
+                    output_file = f"{output_dir}/part_{part_ind}.arrow"
+                    logging.info("Output file is ", output_file)
+                    # mkdir -p
+                    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+                    # just keep main data
+                    fields_to_keep = ["text", "id", "lang"]
+                    all_texts = [
+                        {k: v for k, v in line.items() if k in fields_to_keep}
+                        for line in all_texts
+                    ]
+                    df = pd.DataFrame(all_texts)
+                    df.to_parquet(output_file)
+                    logging.info(
+                        f"Wrote dataset of size {current_tokens} to {output_file}"
+                    )
+
+                    del df
+                    all_texts = []
+
+    logging.info(f"Saved all output ({current_tokens} tokens)")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_tokens", help="Number of tokens to fetch", type=int)
-    parser.add_argument("--output", help="Output file", type=str)
+    parser.add_argument(
+        "--num_tokens",
+        help="Number of tokens to fetch. You can also write xB/xM to fetch x billions/millions",
+        type=str,
+    )
+    parser.add_argument(
+        "--num_total_tokens",
+        help="Total number of tokens to fetch. You can also write xB/xM to fetch x billions/millions",
+        type=str,
+    )
+    parser.add_argument("--output", help="Output dir", type=str)
     parser.add_argument(
         "--domain",
         help="Domains to fetch",
@@ -48,83 +149,58 @@ if __name__ == "__main__":
         ],
     )
     args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
 
-    output_file = (
-        args.output
-        if args.output
-        else f"./{args.domain}_{args.num_tokens}_tokens.arrow"
-    )
-    current_tokens = 0
-    num_tokens = (
-        args.num_tokens if args.num_tokens else TOKENS_TO_FETCH_10B[args.domain]
-    )
+    if args.num_tokens and args.num_total_tokens:
+        raise ValueError(
+            "Please specify only one of --num_tokens or --num_total_tokens (num_tokens is per domain, while num_total will calculate the appropriate number for a domain based on the total)"
+        )
 
-    print(f"Fetching {num_tokens} tokens from {args.domain}")
+    if args.num_tokens:
+        logging.info(f"Fetching {args.num_tokens} tokens")
+        num_tokens = parse_num(args.num_tokens)
+    elif args.num_total_tokens:
+        logging.info("Total domain tokens not specified, using 10B ratio mix")
+        num_tokens = (
+            int(
+                (
+                    (parse_num(args.num_total_tokens) / 10_000_000_000)
+                    * TOKENS_TO_FETCH_10B[args.domain]
+                )
+            )
+            // 1_000_000
+        ) * 1_000_000
+    else:
+        logging.info("Total tokens/domain tokens not specified, using 10B mix")
+        num_tokens = TOKENS_TO_FETCH_10B[args.domain]
+
     # the flask server has to be up on clio
     all_files_lst = requests.get("http://128.2.209.71:5000/list-all").json()
-
-    # shuffle
-    random.seed(42)
-    random.shuffle(all_files_lst)
-    all_texts = []
-
-    # filter out non-gz files
-    all_files_lst = [f for f in all_files_lst if f.endswith(".gz")]
-
-    # filter by top level domain
-    all_files_lst = [f for f in all_files_lst if args.domain in f]
-    tokenizer = AutoTokenizer.from_pretrained(LLAMA_DIR)
-    tokenizer.pad_token = tokenizer.eos_token
-
-    i = 0
-    part_ind = 0
-    with tqdm(total=num_tokens) as pbar:
-        while current_tokens < num_tokens:
-            response = requests.get(f"http://128.2.209.71:5000/{all_files_lst[i]}")
-            if response.status_code != 200:
-                print(f"Error fetching {all_files_lst[i]}")
-                continue
-
-            i += 1
-            lines = [json.loads(l) for l in process_zipped_file(response.content)]
-            texts = [line["text"] for line in lines]
-            all_texts.extend(lines)
-            # tokenizing individually to avoid oom
-            for text in texts:
-                encoded_inputs = tokenizer(
-                    text, truncation=True, padding=True, return_tensors="pt"
+    if args.domain:
+        fetch_tokens(
+            num_tokens=num_tokens,
+            domain=args.domain,
+            output_dir=args.output,
+            all_files_lst=all_files_lst,
+        )
+    else:
+        logging.info(
+            "Fetching from all domains, num_total_tokens will be used, following the 10B ratio mix"
+        )
+        for domain in TOKENS_TO_FETCH_10B.keys():
+            logging.info(f"Fetching {domain}")
+            num_tokens = (
+                int(
+                    (
+                        (parse_num(args.num_total_tokens) / 10_000_000_000)
+                        * TOKENS_TO_FETCH_10B[args.domain]
+                    )
                 )
-                num_non_padding_toks = (
-                    encoded_inputs["attention_mask"].sum(dim=1).tolist()
-                )
-                current_tokens += sum(num_non_padding_toks)
-                pbar.update(sum(num_non_padding_toks))
-
-                if current_tokens >= num_tokens:
-                    break
-
-            # save the reduced dataset as an arrow file, dump every 1M lines
-            if current_tokens >= num_tokens or len(all_texts) >= 1_000_000:
-                part_ind += 1
-                output_file = (
-                    args.output
-                    if args.output
-                    else f"./{args.domain}_{num_tokens}/part_{part_ind}.arrow"
-                )
-                # mkdir -p
-                os.makedirs(os.path.dirname(output_file), exist_ok=True)
-
-                # just keep main data
-                fields_to_keep = ["text", "id", "lang"]
-                all_texts = [
-                    {k: v for k, v in line.items() if k in fields_to_keep}
-                    for line in all_texts
-                ]
-                breakpoint()
-                df = pd.DataFrame(all_texts)
-                df.to_parquet(output_file)
-                print(f"Wrote dataset of size {current_tokens} to {output_file}")
-
-                all_texts = []
-
-    print(f"Saved all output ({current_tokens} tokens)")
+                // 1_000_000
+            ) * 1_000_000
+            fetch_tokens(
+                num_tokens=num_tokens,
+                domain=domain,
+                output_dir=args.output,
+                all_files_lst=all_files_lst,
+            )
--- demo_scripts/get_tokens.sh
+++ demo_scripts/get_tokens.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=get_tokens
-#SBATCH --output=get_tokens.out
-#SBATCH --cpus-per-task=30
-#SBATCH --mem=30G
+#SBATCH --job-name=get_tokens_wiki
+#SBATCH --output=get_tokens_wiki.out
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --mem=75G
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=emmy@cmu.edu
+
+domain=${1:-gutenberg-books}
+num_total_tokens=${2:-100B}
+base_dir=${3:-/data/tir/projects/tir3/users/mengyan3/dolma_100B}
+
+output="${base_dir}/${domain}"
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate towerllm-env
@@ -13,8 +19,4 @@ conda activate towerllm-env
 set -euo pipefail
 
 # delete intermediate files immediately so we don't run out of space
-python my_scripts/fetch_tokens_from_dolma.py --domain gutenberg-books
-python my_scripts/fetch_tokens_from_dolma.py --domain peS2o
-#python my_scripts/fetch_tokens_from_dolma.py --domain c4
-#python my_scripts/fetch_tokens_from_dolma.py --domain stack-code
-#python my_scripts/fetch_tokens_from_dolma.py --domain common-crawl
+python demo_scripts/fetch_tokens_from_dolma.py --domain $domain --num_total_tokens $num_total_tokens --output $output
