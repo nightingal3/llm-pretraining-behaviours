0c801a0b875d021e76e0adaa7e233247670d2d71
Amanda Bertsch <42593540+abertsch72@users.noreply.github.com>
2024-03-25 16:27:32 -0400

    add outpath, arg help

--- demo_scripts/hf_eval_harness.sh
+++ demo_scripts/hf_eval_harness.sh
@@ -1,10 +1,18 @@
+if [[ "${1:-}" == "-h" ]] || [[ "${1:-}" == "--help" ]]; then
+   echo "Usage: bash hf_eval_harness [model_path] [output_path]"
+   exit 0
+fi
+
 MODELPATH=$1
+OUTPATH=$2
 
 lm_eval --model hf \
     --model_args pretrained=$MODELPATH \
     --tasks lambada_openai,lambada_standard,babi,gsm8k_cot_self_consistency,gsm8k_cot,fld,math_word_problems,asdiv,logiqa2,scrolls \
     --device cuda:0 \
-    --batch_size auto
+    --batch_size auto \
+    --output_path $2
+
     
 #note: `math_word_problems` encompasses Minerva Math, MathQA, GSM8k (no CoT) and a few others.
 
