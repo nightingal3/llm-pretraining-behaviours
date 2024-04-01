6de991a1f79b15082399f7cdb65c92edadf42cf6
nightingal3 <emmy.liu.1998@gmail.com>
2024-04-01 15:32:53 -0400

    Improve xgboost code (#67)
    * improve xgboost code
    
    * encode categorical vars
    
    * fix all setting and other things in collect features
    
    * add more documentation
    
    * Update performance_prediction/collect_features_tocsv.py
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    
    ---------
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>

--- performance_prediction/collect_features_tocsv.py
+++ performance_prediction/collect_features_tocsv.py
@@ -31,8 +31,10 @@ def extract_features_from_json(json_data, features):
     extract(json_data, features)
     for feature in features:
         if feature not in extracted_features:
-            print(feature, " was not found so adding default value of 0!")
-            extracted_features[feature] = 0
+            print(feature, " was not found so adding default value of -1!")
+            extracted_features[feature] = (
+                -1
+            )  # using -1 as default val since a model could score 0 for real
 
     extracted_features = {
         key: extracted_features[key] for key in sorted(extracted_features)
@@ -41,38 +43,134 @@ def extract_features_from_json(json_data, features):
     return extracted_features
 
 
-def main(input_dir, output_dir, config_path, type_selection):
+def normalize_features(row: pd.Series):
+    """
+    Puts features from scores into a standardized format, since they are dicts inside the dataset's column.
+    Ignores columns that are not dicts.
 
+    Args:
+        row (pd.Series): The row to normalize.
+
+    Returns:
+        dict: The normalized features.
+
+    Example:
+        Given a row with the column "drop_3_shot", which has a dict {"acc": 0.5, "acc_stderr": 0.1},
+        the output will be {"drop_3_shot_acc": 0.5, "drop_3_shot_acc_stderr": 0.1}.
+    """
+    normalized_features = {}
+    breakpoint()
+    for key, value in row.items():
+        if isinstance(value, dict):
+            for subkey, subvalue in value.items():
+                normalized_features[f"{key}_{subkey}"] = subvalue
+        else:
+            normalized_features[key] = value
+
+    return normalized_features
+
+
+def main(
+    input_dir: str,
+    output_dir: str,
+    config_path: str,
+    type_selection: str,
+    dataset: str = "all",
+    n_shots: int = -1,
+):
     with open(config_path, "r") as config_file:
         config = json.load(config_file)
     features_to_extract = config[type_selection]
 
+    extracted_features_all = []
     for input_file in os.listdir(input_dir):
+        input_file_path = os.path.join(input_dir, input_file)
 
-        input_file = os.path.join(input_dir, input_file)
-        output_file = os.path.join(output_dir, f"training_{type_selection}_final.csv")
-        # Load the JSON file into a dictionary
-        with open(input_file, "r") as file:
+        with open(input_file_path, "r") as file:
             json_data = json.load(file)
 
-        extracted_features = extract_features_from_json(json_data, features_to_extract)
-        # Display the extracted features
+        # the processing is pretty different between scores and model/dataset features, so separating them
         if type_selection == "score":
-            if extracted_features["hellaswag"]:
-                extracted_features["hellaswag"] = extracted_features["hellaswag"][
-                    "10-shot"
-                ]["acc"]
+            if dataset == "all":
+                for _, ds_value in json_data.get("results", {}).items():
+                    extracted_features = process_dataset_scores(
+                        input_file, ds_value, n_shots
+                    )
+                    if extracted_features:
+                        extracted_features["model_name"] = json_data["model_name"]
+                        extracted_features_all.append(extracted_features)
+            else:
+                ds_value = json_data.get("results", {}).get(dataset, {})
+                extracted_features = process_dataset_scores(
+                    input_file, ds_value, n_shots
+                )
+                if extracted_features:
+                    extracted_features["model_name"] = json_data["model_name"]
+                    extracted_features_all.append(extracted_features)
+        else:
+            extracted_features = extract_features_from_json(
+                json_data, features_to_extract
+            )
+            extracted_features_all.append(extracted_features)
 
-        df = pd.DataFrame([extracted_features])
-        file_exists = os.path.exists(output_file)
-        # Write the DataFrame to a CSV file
-        df.to_csv(output_file, mode="a", index=False, header=not file_exists)
+    df = pd.DataFrame(extracted_features_all)
+
+    if type_selection == "score":
+        features_df = df.drop(columns=["model_name"]).apply(
+            normalize_features, axis=1, result_type="expand"
+        )
+        final_df = pd.concat([df[["model_name"]], features_df], axis=1)
+    else:
+        final_df = df
+
+    output_file = os.path.join(output_dir, f"training_{type_selection}_final.csv")
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    final_df.to_csv(output_file, index=False)
+    print(f"Features extracted and saved to {output_file}")
+
+
+def process_dataset_scores(input_file: str, ds_value: dict, n_shots: int = -1):
+    """
+    Processes dataset performance results, handling n-shot scenarios.
+
+    Args:
+        input_file (str): The name of the input file.
+        ds_value (dict): The model's performance results across datasets.
+        n_shots (int): The number of shots to use as the model's final result. -1 will attempt to grab all shots available.
+
+    Returns:
+        dict: The extracted features.
+
+    """
+    extracted_features = {}
+
+    for dataset in ds_value:
+        try:
+            if n_shots == -1:
+                avail_shots = list(ds_value[dataset].keys())
+                for n_s in avail_shots:
+                    n_shots_tmp = int(n_s.split("-")[0])
+                    new_key = f"{dataset}_{n_shots_tmp}-shot"
+                    extracted_features[new_key] = ds_value[dataset][
+                        f"{n_shots_tmp}-shot"
+                    ]
+            else:
+                new_key = f"{dataset}_{n_shots}-shot"
+                extracted_features[new_key] = ds_value[dataset][f"{n_shots}-shot"]
+        except KeyError:
+            print(f"{input_file}: {n_shots}-shot results not found in the dataset")
+            continue
+        except:
+            print(f"{input_file}: {dataset} not found in the dataset")
+            continue
+
+    return extracted_features
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input_json",
+        "--input_dir",
         type=str,
     )
     parser.add_argument(
@@ -80,14 +178,36 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument(
-        "--config_file", type=str, required=True, help="Path to the configuration file."
+        "--config_file",
+        type=str,
+        default="./config.json",
+        help="Path to the configuration file.",
     ),
     parser.add_argument(
         "--type",
         type=str,
-        required=True,
-        help="collect model or score or dataset features?",
+        choices=["score", "model"],
+        default="score",
+        help="collect model or score or dataset features",
     )
-
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="all",
+        help="dataset to collect results for. Pass 'all' to collect all datasets",
+    )
+    parser.add_argument(
+        "--n_shots",
+        type=int,
+        default=-1,
+        help="number of shots to collect results for. -1 will attempt to grab all shots available.",
+    )
     args = parser.parse_args()
-    main(args.input_json, args.output_dir, args.config_file, args.type)
+    main(
+        args.input_dir,
+        args.output_dir,
+        args.config_file,
+        args.type,
+        args.dataset,
+        args.n_shots,
+    )
