4861b8eb1123f6f1d8dff38d289020f1b3c42406
Michael Chen <chen.michael.1116@gmail.com>
2024-04-19 20:27:51 -0400

    add back top_langs_asts.py, black format

---
+++ dolma_data_processing/code_features/top_langs_asts.py
@@ -0,0 +1,9 @@
+import os
+
+langs = os.listdir(
+    "/data/tir/projects/tir7/user_data/mchen5/llm-pretraining-behaviours/dolma_data_processing/code_features/ast_testing/test_code"
+)
+for lang in ["python"]:
+    os.system(
+        f"python3 ast_features.py --lang={lang} --input_file=ast_testing/test_code/{lang} --output_file=ast_testing/output/{lang}"
+    )
